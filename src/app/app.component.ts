1a4a96280637b986997388e05aa2662d71e957fd
merobal <facebook@merobal.hu>
2020-04-01 20:36:00 +0200

    Fix formio setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   builder = true;
   form: any = {};
+  private _form: any = {};
 
   onChange(event: any) {
     console.log(event);
     if (['addComponent', 'saveComponent', 'deleteComponent'].indexOf(event.type) > -1) {
-      this.form = event.form;
+      this._form = cloneDeep(event.form);
     }
+  }
+
+  toggleBuilder() {
+    this.builder = !this.builder;
+    this.form = cloneDeep(this._form);
   }
 }
